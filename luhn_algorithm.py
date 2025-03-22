90842081c2769b82966203fd5a098e1a9991f28b
Mohammed Attya <m.attya@skopenow.com>
2025-03-22 03:29:17 +0200

    enhance match calculations

--- luhn_algorithm.py
+++ luhn_algorithm.py
@@ -1,18 +1,14 @@
 def verify(digits):
-    sum = 0
     digits = [int(i) for i in digits]
-    first = digits.pop()
+    sum = digits.pop()
     i = 0
     for d in digits[::-1]:
-        muliplier = 1
-        if i % 2 == 0:
-            muliplier = 2
+        muliplier = 2 - (i % 2)
         x = int(d) * muliplier
-        if x > 9:
-            x -= 9
-        sum += x
+        sum += x + x // 10
         i += 1
 
-    return first == ((10 - (sum % 10)) % 10)
+    return sum % 10 == 0
 
-print(verify('17893729974'))
+assert verify('17893729974')
+assert not verify('17893729973')
