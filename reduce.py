51f8f7f523393666fdb403ed993aa809479b0f82
Mohammed Attya <m.attya@skopenow.com>
2025-03-26 02:49:19 +0200

    implement reduce alongside other functions

---
+++ reduce.py
@@ -0,0 +1,85 @@
+def reduce(f, xs, init=None):
+    """
+    Apply the function f cummulatively to the items of xs, reducing to a single value.
+
+    If initializer is provided, use it as the first value. Otherwise, use only the
+    values in xs.
+
+    >>> reduce(lambda acc, x: acc + x, [1, 2, 3, 4])
+    10
+    >>> def mirror(d, x):
+    ...     d[x] = x
+    ...     return d
+    >>> reduce(mirror, ['foo', 'bar'], {})
+    {'foo': 'foo', 'bar': 'bar'}
+    """
+    if init is None:
+        init = xs.pop(0)
+    for x in xs:
+        init = f(init, x)
+    return init
+
+
+def product(nums):
+    """
+    Return the product of the given numbers
+
+    >>> product([2, 3])
+    6
+    >>> product([-1.0, -1.0, -1.0])
+    -1.0
+    >>> product([])
+    1
+    """
+    return reduce(lambda x, y: x * y, nums, 1)
+
+
+def my_map(f, xs):
+    """
+    Return a new list, with the function f applied to each item in the given list
+
+    >>> my_map(lambda x: x * x, [1, 2, 3, 4])
+    [1, 4, 9, 16]
+    """
+    return [f(x) for x in xs]
+
+
+def my_filter(f, xs):
+    """
+    Given a predicate function f (a function which returns True or False) and a list
+    xs, return a new list with only the items of xs where f(x) is True
+
+    >>> my_filter(lambda x: x > 0, [-1, 3, -2, 5])
+    [3, 5]
+    >>> my_filter(lambda x: False, [1, 2])
+    []
+    """
+    ls = []
+    for x in xs:
+        if f(x):
+            ls.append(x)
+    return ls
+
+
+def my_zip(*iters):
+    """
+    Given one or more iterables of the same length, return a list of lists of them
+    "zipped" together, ie grouped by index
+
+    >>> my_zip('abc', 'def', (1, 2, 3))
+    [['a', 'd', 1], ['b', 'e', 2], ['c', 'f', 3]]
+    """
+    ls = []
+    for i in range(len(iters[0])):
+        ls.append([])
+    for iter in iters:
+        i = 0
+        for x in iter:
+            ls[i].append(x)
+            i += 1
+    return ls
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+    print('ok')
